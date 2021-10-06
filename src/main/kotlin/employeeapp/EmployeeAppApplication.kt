7fd68a50b70f0b5916578496c1537d2bea8b9976
aplans <aplans@ingeniamc.com>
2021-10-06 13:05:53 +0200

    Added basic architecture

---
+++ src/main/kotlin/employeeapp/EmployeeAppApplication.kt
@@ -0,0 +1,11 @@
+package employeeapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmployeeAppApplication>(*args)
+}
