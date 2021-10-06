313c64d3238dcba11339954b8185725fb4ce505c
aplans <aplans@ingeniamc.com>
2021-10-06 17:23:40 +0200

    Add repository definition

---
+++ src/main/kotlin/employeeapp/EmployeeRepository.kt
@@ -0,0 +1,8 @@
+package employeeapp
+
+import employeeapp.repository.model.Employee
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface EmployeeRepository : JpaRepository<Employee, Long>
